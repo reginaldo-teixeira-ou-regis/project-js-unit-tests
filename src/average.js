5c3afaaafddd3591edfd1ae4a050ec676ddc8e68
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-09-20 18:50:54 -0300

    Requisit 1 complete

--- src/average.js
+++ src/average.js
@@ -9,7 +9,37 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+const isNumberOrIsEmpty = (array) => {
+  for (let i of array) {
+    if (typeof i !== 'number') {
+      throw new Error(undefined);
+    }
+  }
+  if (array.length === 0) {
+    throw new Error(undefined);
+  }
+};
 
-const average = () => {};
+const average = (array) => {
+  try {
+    isNumberOrIsEmpty(array);
+    let sum = 0;
+    let result;
+    for (let count of array) {
+      sum += count;
+    }
+    result = sum / array.length;
+    return Math.round(result);
+  } catch (error) {
+    return undefined;
+  }
+};
+
+console.log(average([4, 2, 6, 3, 7, 1]));
+console.log(average([0, 2, 6, 3, 7, 1]));
+console.log(average([4, 2, '', 3, 7, 1]));
+console.log(average([4, 2, '1', 3, 7, 1]));
+console.log(average([4, 2, 'r', 3, 7, 1]));
+console.log(average([]));
 
 module.exports = average;
